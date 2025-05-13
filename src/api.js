e9790917f4a57d169e92a64170418e5955608877
Alfar1to <andreialfaro07@gmail.com>
2025-05-13 13:34:51 -0700

    ash

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const searchImages = async (term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=vJOjNnTHHzet-mHoGsYVu9tka_u0LNVpe79bESrDaSE'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=m6dfGwPvs0ywEzGEmcNut8QVdT9GLOwZg06_LMMq3Z0'
 
     const response = await axios.get(url, {
         params:{
